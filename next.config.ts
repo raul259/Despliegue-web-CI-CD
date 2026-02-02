3cff662f67009570fb683956aae0c6f4a39b3339
raul259 <raaulito5@gmail.com>
2026-02-02 11:31:42 +0100

    config: ajustar basePath para despliegue en Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', 
-  basePath: '/Despliegue-web-CI-CD', 
+  basePath: '/', 
   images: {
     unoptimized: true, 
   },
