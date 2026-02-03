71b73e90c670440b5e2763ebf4d692ec0fac392d
raul259 <raaulito5@gmail.com>
2026-02-03 02:43:33 +0100

    cambios de deploy y next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithub = process.env.DEPLOY_TARGET === "github";
+const repo = "ejercicio-alumnos";
+
 const nextConfig: NextConfig = {
-  output: 'export', 
-  basePath: '', 
-  images: {
-    unoptimized: true, 
-  },
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  ...(isGithub
+    ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` }
+    : {}),
 };
 
 export default nextConfig;
