5082296321a8b862e2c81ad2e46814436b8faee5
raul259 <raul259@users.noreply.github.com>
2026-02-03 01:44:31 +0000

    deploy: 71b73e90c670440b5e2763ebf4d692ec0fac392d

--- _next/static/8i6R8RHC4KHGBTHbKFCZj/_buildManifest.js
+++ _next/static/8i6R8RHC4KHGBTHbKFCZj/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ejercicio-alumnos//_next/:path+",
+        "destination": "/ejercicio-alumnos/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
